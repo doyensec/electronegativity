b052185da964f7032d6d406804519b042578b198
Ibram Marzouk <ibram.marzouk@gmail.com>
2018-08-26 02:33:28 -0400

    remove legacy component

--- src/finder/dom.js
+++
@@ -1,8 +0,0 @@
-import logger from 'winston';
-
-export class Dom {
-  static calculatePosition(dom, element) {
-    // TODO try to use algorithm from https://github.com/keeganstreet/domtosource
-    return { line: 0, column: 0 };
-  }
-}
--- src/finder/index.js
+++ src/finder/index.js
@@ -1,10 +1,5 @@
 import { Finder } from './finder';
 import { Ast } from './ast';
-import { Dom } from './dom';
 
-// module.exports.Check = Check;
-// module.exports.JavaScriptCheck = JavaScriptCheck;
-// module.exports.HTMLCheck = HTMLCheck;
 module.exports.Finder = Finder;
 module.exports.Ast = Ast;
-module.exports.Dom = Dom;
