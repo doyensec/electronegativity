9f0da209fb7d13d95f94f73a01b63fb4049dcc76
jl <jarlob@gmail.com>
2018-10-02 11:32:18 +0300

    Handle shebang files

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -8,6 +8,37 @@ export class Parser {
   constructor() { }
 
   parse(filename, content) {
+    // https://en.wikipedia.org/wiki/Shebang_(Unix)
+    function strip_shebang(content) {
+      let i = 0;
+      for (; i < content.length; ++i) {
+        if (content[i] !== ' ' && content[i] !== '\t' && content[i] !== '\n' && content[i] !== '\r')
+          break;
+      }
+  
+      let start = -1;
+      let end = -1;
+      if (content.length >= 2 + i && content[i] === '#' && content[i + 1] === '!') {
+        start = i;
+        for (; i < content.length; ++i) {
+          if (content[i] === '\n' || content[i] === '\r') {
+            end = i;
+            break;
+          }
+        }
+
+        if (start !== -1) {
+          if (end === -1) {
+            return ''; // shouldn't really happen unless the only line in a file is the shebang
+          }
+
+          // replace with spaces to keep offsets intact
+          return content.slice(0, start) + ' '.repeat(end - start) + content.slice(end);
+        }
+      }
+      return content;
+    }
+
     const ext = extension(filename);
 
     const sourceType = sourceExtensions[ext];
@@ -16,7 +47,7 @@ export class Parser {
     switch (sourceType) {
       case sourceTypes.JAVASCRIPT:
         try{
-          data = esprima_parse(content, { loc: true, tolerant: true });
+          data = esprima_parse(strip_shebang(content), { loc: true, tolerant: true });
         }catch(e){
           data = '';
         }
