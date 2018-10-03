69df7d2ae25f964cdd0820374b1c7bf1fc721615
jl <jarlob@gmail.com>
2018-10-03 23:49:25 +0300

    False negative fix in HttpResourcesCheck: handle `HTTP://${url}`

--- src/finder/checks/HTTPResourcesCheck.js
+++ src/finder/checks/HTTPResourcesCheck.js
@@ -10,12 +10,23 @@ export default class HTTPResourcesCheck {
   match(data) {
     if (data.type !== 'CallExpression') return null;
     if (!(data.callee.property && data.callee.property.name === "loadURL")) return null;
-    let location;
-    
-    if(data.arguments[0].type == "Literal" && data.arguments[0].value.startsWith("http://")){
-        location = { line: data.loc.start.line, column: data.loc.start.column };
+
+    switch (data.arguments[0].type) {
+      case "Literal":
+        if (!data.arguments[0].value.toUpperCase().startsWith("HTTP://"))
+          return undefined;
+        break;
+      case "TemplateLiteral":
+        if (data.arguments[0].type !== "TemplateLiteral" ||
+            data.arguments[0].quasis[0].type !== "TemplateElement" ||
+            !data.arguments[0].quasis[0].value.cooked.toUpperCase().startsWith("HTTP://"))
+          return undefined;
+        break;
+      default:
+        return undefined;
     }
     
+    let location = { line: data.loc.start.line, column: data.loc.start.column };   
     return location;
   }
 }
