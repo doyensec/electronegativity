834422050d04ae59674589e202fe66f11bfef9a5
phosphore <lorenzo.stl@gmail.com>
2019-03-28 17:05:54 +0100

    EXPERIMENTAL_FEATURES_JS_CHECK support for BrowserWindow config variables and tests

--- src/finder/checks/ExperimentalFeaturesJSCheck.js
+++ src/finder/checks/ExperimentalFeaturesJSCheck.js
@@ -7,14 +7,21 @@ export default class ExperimentalFeaturesJSCheck {
     this.type = sourceTypes.JAVASCRIPT;
   }
 
-  match(astNode, astHelper){
+  match(astNode, astHelper, scope){
     if (astNode.type !== 'NewExpression') return null;
     if (astNode.callee.name !== 'BrowserWindow') return null;
 
     let location = [];
 
     if (astNode.arguments.length > 0) {
-      const found_nodes = astHelper.findNodeByType(astNode.arguments[0],
+
+      var target = {};
+      if (scope.resolveVarValue)
+        target = scope.resolveVarValue(astNode);
+      else
+        target = astNode.arguments[0];
+
+      const found_nodes = astHelper.findNodeByType(target,
         astHelper.PropertyName,
         astHelper.PropertyDepth,
         false,
@@ -24,7 +31,7 @@ export default class ExperimentalFeaturesJSCheck {
                  node.key.name  === 'experimentalCanvasFeatures'));
 
       for (const node of found_nodes) {
-        if (node.value.value === true) {
+        if (node.value.value == true) {
           location.push({ line: node.key.loc.start.line, column: node.key.loc.start.column, id: this.id, description: this.description, manualReview: false });
         }
       }
