3c161fa6b3aa67d15d3349efac1c733c84f6d344
phosphore <lorenzo.stl@gmail.com>
2019-04-10 17:57:23 +0200

    Introduce AVAILABLE_SECURITY_FIXES_GLOBAL_CHECK, resolves #30

--- src/finder/checks/ElectronVersionJSONCheck.js
+++ src/finder/checks/ElectronVersionJSONCheck.js
@@ -1,22 +1,11 @@
-import linenumber from 'linenumber';
-import fs from 'fs';
-import path from 'path';
-import os from 'os';
-import { lt, coerce, major, minor } from 'semver';
-import got from 'got';
-import chalk from 'chalk';
-
+import { coerce } from 'semver';
 import { sourceTypes } from '../../parser/types';
-
-let releases;
 
 export default class ElectronVersionJSONCheck {
   constructor() {
     this.id = 'ELECTRON_VERSION_JSON_CHECK';
-    this.description = `Keep dependencies up-to-date. The electron version used is outdated.`;
+    this.description = `Gets the electron version used by inspecting the package.json file.`;
     this.type = sourceTypes.JSON;
-    this.versionsUrl = 'https://raw.githubusercontent.com/doyensec/electronegativity/master/safe_releases.json';
-    this.cacheFileName = 'electronegativity_safe_releases.json';
   }
 
   async match(content){
@@ -24,47 +13,15 @@ export default class ElectronVersionJSONCheck {
     const electronDev = content.json.devDependencies && 'electron' in content.json.devDependencies ? coerce(content.json.devDependencies.electron) : undefined;
 
     let location = [];
-    if (electron || electronDev) {
-      if (!releases) {
-        try {
-          const response = await got(this.versionsUrl, { json: true });
-          releases = response.body;
-          fs.writeFileSync(path.join(os.tmpdir(), this.cacheFileName), JSON.stringify(releases));
-        } catch (error) {
-          let cache = path.join(os.tmpdir(), this.cacheFileName);
-
-          const devPath = path.resolve('safe_releases.json');
-          if (fs.existsSync(devPath))
-            cache = devPath;
-
-          console.log(chalk.yellow(`Failed to download ${this.versionsUrl}. Reading from ${cache}.`));
-          releases = JSON.parse(fs.readFileSync(cache));
-        }
-      }
-
-      this.checkVersion(content, location, electron);
-      this.checkVersion(content, location, electronDev);
+    if (electron) {
+      location.push({ line: 1, column: 0, id: this.id, description: this.description, properties: { "versionNumber": electron.raw }, manualReview: false });
     }
+
+    if (electronDev) {
+      location.push({ line: 1, column: 0, id: this.id, description: this.description, properties: { "versionNumber": electronDev.raw }, manualReview: false });
+    }
+
     return location;
   }
 
-  checkVersion(data, location, version) {
-    if (version && this.isVulnerableVersion(version)) {
-      let matches = linenumber(data.text, /(?:"electron"|electron)\s*:\s*.*[,}]?/g);
-      for (const m of matches) {
-        if (m.match.includes(version.raw))
-          location.push({ line: m.line, column: 0, id: this.id, description: this.description, manualReview: false });
-      }
-    }
-  }
-
-  isVulnerableVersion(version) {
-    const majorVersion = major(version);
-    const minorVersion = minor(version);
-    for (const safe of releases.safe) {
-      if (major(safe) === majorVersion && minor(safe) === minorVersion)
-        return lt(version, safe);
-    }
-    return true;
-  }
 }
--- src/finder/checks/GlobalChecks/index.js
+++ src/finder/checks/GlobalChecks/index.js
@@ -1,13 +1,15 @@
 import AffinityGlobalCheck from './AffinityGlobalCheck';
+import AvailableSecurityFixesGlobalCheck from './AvailableSecurityFixesGlobalCheck';
 import CSPGlobalCheck from './CSPGlobalCheck';
 import LimitNavigationGlobalCheck from './LimitNavigationGlobalCheck';
 import PermissionRequestHandlerGlobalCheck from './PermissionRequestHandlerGlobalCheck';
 
 const GLOBAL_CHECKS = [
   AffinityGlobalCheck,
+  AvailableSecurityFixesGlobalCheck,
   CSPGlobalCheck,
   LimitNavigationGlobalCheck,
-	PermissionRequestHandlerGlobalCheck
+  PermissionRequestHandlerGlobalCheck
 ];
 
 module.exports.GLOBAL_CHECKS = GLOBAL_CHECKS;
