02d9ea101e814b7f08cd1e70ce2bf5eb926e56d5
phosphore <lorenzo.stl@gmail.com>
2019-10-28 15:04:19 +0100

    add support for nodeintegrationinsubframes in HTML, close #52

--- src/finder/checks/AtomicChecks/NodeIntegrationHTMLCheck.js
+++ src/finder/checks/AtomicChecks/NodeIntegrationHTMLCheck.js
@@ -14,7 +14,9 @@ export default class NodeIntegrationHTMLCheck {
     const self = this;
     webviews.each(function (i, elem) {
       const nodeintegration = cheerioObj(this).attr('nodeintegration');
-      if (nodeintegration !== undefined) {
+      const nodeintegrationInSubframes = cheerioObj(this).attr('nodeintegrationinsubframes');
+
+      if (nodeintegration !== undefined || nodeintegrationInSubframes !== undefined) {
         loc.push({ line: content.substr(0, elem.startIndex).split('\n').length, column: 0, id: self.id, description: self.description, severity: severity.HIGH, confidence: confidence.FIRM, manualReview: false });
       }
     });
