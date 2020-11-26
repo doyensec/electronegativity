bcae6bb6ae09e08ce49bca6b816d91076b0d2a9c
phosphore <lorenzo.stl@gmail.com>
2020-11-26 18:37:50 +0100

    address #75 comments

--- src/finder/checks/AtomicChecks/ContextIsolationJSCheck.js
+++ src/finder/checks/AtomicChecks/ContextIsolationJSCheck.js
@@ -18,11 +18,11 @@ export default class ContextIsolationJSCheck {
 
       var target = scope.resolveVarValue(astNode);
 
-      astHelper.findNodeByType(target,
-        astHelper.PropertyName,
-        astHelper.PropertyDepth,
-        true, // any preload is enough
-        node => (node.key.value === 'preload' || node.key.name === 'preload'));
+      // astHelper.findNodeByType(target,
+      //   astHelper.PropertyName,
+      //   astHelper.PropertyDepth,
+      //   true, // any preload is enough
+      //   node => (node.key.value === 'preload' || node.key.name === 'preload'));
 
       const contextIsolation = astHelper.findNodeByType(target,
         astHelper.PropertyName,
@@ -34,10 +34,16 @@ export default class ContextIsolationJSCheck {
       //if (preload.length > 0) { 
       if (contextIsolation.length > 0) {
         for (const node of contextIsolation) {
+
+          if (node.value.type === "Identifier") {
+            var target = scope.getVarInScope(node.value.name);
+            if ((!target || target.defs.length == 0 || !target.defs[0].node.init || !target.defs[0].node.init.value) || // e.g. var variable; declared but not assigned or assigned later in an undefined way
+                (target && target.defs[0].node.init && target.defs[0].node.init.value !== true)) // e.g. var variable = true; declared and assigned on creation, the only case we can afford to detect atm
+               location.push({ line: node.key.loc.start.line, column: node.key.loc.start.column, id: this.id, description: this.description, shortenedURL: this.shortenedURL, severity: severity.HIGH, confidence: confidence.FIRM, manualReview: false });
+          } else if(node.value.value !== true) {
           // in practice if there are two keys with the same name, the value of the last one wins
           // but technically it is an invalid json
           // just to be on the safe side show a warning if any value is insecure
-          if(node.value.value !== true) {
             location.push({ line: node.key.loc.start.line, column: node.key.loc.start.column, id: this.id, description: this.description, shortenedURL: this.shortenedURL, severity: severity.HIGH, confidence: confidence.FIRM, manualReview: false });
           }
         }
