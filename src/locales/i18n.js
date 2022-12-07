44c0b74b7a3389031341db1d5def22979d4cab24
phosphore <lorenzo.stl@gmail.com>
2022-12-07 11:25:18 +0100

    add widespread locales locally

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -5,7 +5,7 @@ export default function _i18n () {
   return new Promise((resolve, reject) => {
 	var defaultLocale = "en-US";
 	i18n.configure({
-	  locales: ['en-US'], // widespread locales available locally
+	  locales: ['en-US', 'es-ES', 'fr-FR'], // some widespread locales available locally
 	  defaultLocale: defaultLocale,
 	  directory: __dirname,
 	  updateFiles: false,
@@ -16,17 +16,18 @@ export default function _i18n () {
 
   	defaultLocale = process.env.LANG ? process.env.LANG : defaultLocale;
   	const i18nSource = require('../../package.json').i18nSource;
-	return got(`${i18nSource}/${defaultLocale}.json`)
+	got(`${i18nSource}/${defaultLocale}.json`, {timeout: {request: 1000}})
 	.then(r => {
 	  if (r.statusCode === 200) {// Add JSON translations to i18n
 	  	i18n.addLocale(defaultLocale, JSON.parse(r.body));
 	  	i18n.setLocale(defaultLocale);
-	  	resolve(); // resolve the promise when the setLocale is set
 	  }
+	  resolve();  // resolve the promise
 	})
 	.catch(e => {
-	  // Could not retrieve updated translations for the current locale
-	  reject(e);
+	  console.log("Could not retrieve updated translations for the current locale");
+	  i18n.setLocale(defaultLocale);
+	  resolve();
 	});
   });
 };
